ff9cdd57b85fd44e9deada8dbd35e678be164c4b
TejShind <tej.gholap25@gmail.com>
2022-02-23 08:30:24 +0530

    [Tejaswini]ADDED:Feature for Welcome to the Snake and Ladder Game!

---
+++ src/com/blz/snakeladder/SnakeLadderGame.java
@@ -0,0 +1,9 @@
+package com.blz.snakeladder;
+
+public class SnakeLadderGame {
+    
+    public static void main(String []args){
+
+        System.out.println(" Welcome to the Snake and Ladder Game");
+    }
+}
