bc3e6a3a07bc86d36fe350a2a4bea72fd8f1d0c3
TejShind <tej.gholap25@gmail.com>
2022-02-23 16:10:42 +0530

    [Tejaswini]ADDED:Feature for Check option for Snake,Ladder and Noplay

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1) Welcome to the Snake and Ladder Game!
 2) Player at Zero position.
 3) Player Roll the Die.
+4) Check fot option No play,Ladder and Snake.
--- src/com/blz/snakeladder/SnakeLadderGame.java
+++ src/com/blz/snakeladder/SnakeLadderGame.java
@@ -1,11 +1,27 @@
 package com.blz.snakeladder;
 
 public class SnakeLadderGame {
+    //CONSTANTS
+    public static final int SNAKE = 1;
+    public static final int LADDER = 2;
 
     public static void main(String[] args) {
-        int startPosition = 0;
-        int dieNumber = (int) Math.floor(Math.random() * 10)%6 + 1;
-        System.out.println(" Player is at start position");
+
+        //VARIABLES
+        int currentPosition = 0;
+        int dieRoll = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        int chooseOption = (int) Math.floor(Math.random() * 10) % 3;
+
+        switch (chooseOption) {
+            case SNAKE -> currentPosition -= dieRoll;
+            case LADDER -> currentPosition += dieRoll;
+            default -> {
+                currentPosition += 0;
+                System.out.println("NO PLAY");
+            }
+
+        }
+        System.out.println("Player is at " + currentPosition + " position");
     }
 }
 
