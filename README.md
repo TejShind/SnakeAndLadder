ca8a379fa74bcefdc8d9e7a712cb8f352c643027
TejShind <tej.gholap25@gmail.com>
2022-02-23 13:12:59 +0530

    [Tejaswini]ADDED:Feature for Player rolls the die to get a number between 1 to 6

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # SnakeAndLadder
 1) Welcome to the Snake and Ladder Game!
 2) Player at Zero position.
+3) Player Roll the Die.
--- src/com/blz/snakeladder/SnakeLadderGame.java
+++ src/com/blz/snakeladder/SnakeLadderGame.java
@@ -4,6 +4,7 @@ public class SnakeLadderGame {
 
     public static void main(String[] args) {
         int startPosition = 0;
+        int dieNumber = (int) Math.floor(Math.random() * 10)%6 + 1;
         System.out.println(" Player is at start position");
     }
 }
